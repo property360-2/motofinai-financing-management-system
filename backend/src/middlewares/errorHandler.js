e333c72013a72e3c0f3602ef85dc7d80f216d9ec
Jun Alvior <junalvior21@gmail.com>
2025-11-01 19:31:05 +0800

    im not stupid or what, im reforging a sword

--- backend/src/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-export default function errorHandler(err, req, res, next) {
-  console.error("❌ Error:", err);
-  res.status(err.status || 500).json({
-    message: err.message || "Internal Server Error",
-  });
-}
--- backend/src/utils/password.js
+++
@@ -1,4 +0,0 @@
-import bcrypt from "bcrypt";
-
-export const hashPassword = async (plain) => await bcrypt.hash(plain, 10);
-export const comparePassword = async (plain, hash) => await bcrypt.compare(plain, hash);
